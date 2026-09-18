2d391c0a158237d203c9edb99c4a2c64db812c69
Suworda <su5200su@gmail.com>
2026-09-18 10:18:52 +0800

    Create Game_Routes.cpp

---
+++ code/Game_Routes.cpp
@@ -0,0 +1,56 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long
+#define ll long long
+
+const int N = 100005;
+const int M = 1e9+7;
+
+struct Graph{
+    int n;
+    vector<vector<int>> pre;
+    vector<bool> vis;
+    vector<int> dp;
+    
+    Graph(int n) : n(n), pre(n+1), vis(n+1), dp(n+1) {}
+
+    void add_edge(int a, int b){
+        pre[b].push_back(a);
+    }
+
+    void dfs(int u){
+        if(vis[u]) return;
+        vis[u] = 1;
+
+        if(u == 1){
+            dp[u] = 1;
+            return;
+        }
+
+        for(int v: pre[u]){
+            dfs(v);
+            dp[u] += dp[v];
+            dp[u] %= M;
+        }
+    }
+};
+
+signed main(){
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    
+    int n,m;
+    cin>>n>>m;
+
+    Graph g(n);
+
+    while(m--){
+        int a,b;
+        cin>>a>>b;
+        g.add_edge(a,b);
+    }
+
+    g.dfs(n);
+
+    cout << g.dp[n] << '\n';
+}
